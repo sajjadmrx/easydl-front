f8c8a3b3860364fb584e191f30a5da27fe22ba1f
mrx <sajjadzibaee102@gmail.com>
2022-05-31 20:56:26 +0430

    fix env

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,11 @@ function App() {
     //     <Route path="/"> <HomePage /> </Route>
     //   </Switch>
     // </Router>
-    // <Switch>
-    //   <Route exact path="/" component={HomePage} loading />
-    //   <Route path="/help" component={HelpPage} loading />
-    //   <Route path="/about" component={AboutPage} loading />
-    // </Switch>
-    <small>You are running this application in <b>{process.env.REACT_APP_API_URL}</b> mode.</small>
-
+    <Switch>
+      <Route exact path="/" component={HomePage} loading />
+      <Route path="/help" component={HelpPage} loading />
+      <Route path="/about" component={AboutPage} loading />
+    </Switch>
   );
 }
 
