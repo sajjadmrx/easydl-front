51d06401ad0afcc9f78f89dadc5aee1af2338a8a
mrx <sajjadzibaee102@gmail.com>
2022-06-05 16:23:05 +0430

    remove Cache File

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,96 +1,24 @@
 // const cacheName = "cache-v1";
 const staticAssets = [
-    './',
-    './index.html',
-    "./logo.png",
-    "./logo16.png",
-    "./logo32.png",
-    "./logo64.png",
-    "./logo192.png",
-    "./logo512.png",
-    "./manifest.json",
-    './brands/rj.png',
-    './brands/soundcloud.png',
-    './brands/spotify.png',
-    './manifest.webmanifest',
-    './amozesh/install.mp4',
-    './amozesh/spotify.mp4'
+
 ]
 
-// self.addEventListener('install', async event => {
-//     const cache = await caches.open(cacheName);
-//     await cache.addAll(staticAssets);
-
-//     return self.skipWaiting();
-
-// });
-
-// self.addEventListener('activate', event => {
-//     self.clients.claim();
-// });
 
 
-// self.addEventListener('fetch', async event => {
 
-//     const req = event.request;
-
-//     const url = new URL(req.url);
-
-//     if (url.origin === location.origin) {
-//         event.respondWith(cacheFirst(req));
-//     } else {
-//         //   event.respondWith(networkAndCache(req));
-//     }
-
-
-// });
-
-// async function cacheFirst(req) {
-//     const cache = await caches.open(cacheName);
-//     const cached = await cache.match(req);
-//     if (cache) return cached;
-//     try {
-//         const networked = await fetch(req);
-//         return networked;
-//     } catch (e) {
-//         console.log(e);
-//         //return await cache.match('/offline.html');
-//     }
-// }
-
-// async function networkAndCache(req) {
-//     const cache = await caches.open(cacheName);
-//     try {
-//         const fresh = await fetch(req, {
-//             headers: {
-//                 'Access-Control-Allow-Origin': '*',
-//                 'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//                 'Access-Control-Allow-Headers': 'Origin, Content-Type, Authorization,Accept',
-//             },
-//         });
-//         await cache.put(req, fresh.clone());
-//         return fresh;
-//     } catch (error) {
-//         const cached = await cache.match(req);
-//         return cached;
-//     }
-// }
-
-
-const PRECACHE = 'precache-v2';
+const PRECACHE = 'v6';
 const RUNTIME = 'runtime';
 
 
-// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(PRECACHE)
             .then(cache => cache.addAll(staticAssets))
             .then(self.skipWaiting())
     );
+
 });
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
     const currentCaches = [PRECACHE, RUNTIME];
     event.waitUntil(
@@ -103,6 +31,7 @@ self.addEventListener('activate', event => {
         }).then(() => self.clients.claim())
     );
 });
+
 
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
