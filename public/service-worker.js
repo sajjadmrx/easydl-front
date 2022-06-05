4bf80bc1c74af6b359fd59cef4bb3897b38a8cd4
mrx <sajjadzibaee102@gmail.com>
2022-06-05 17:32:49 +0430

    add 'amozesh' to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,22 @@
 // const cacheName = "cache-v1";
 const staticAssets = [
-
+    "./logo.png",
+    "./logo16.png",
+    "./logo32.png",
+    "./logo64.png",
+    "./logo192.png",
+    "./logo512.png",
+    './brands/rj.png',
+    './brands/soundcloud.png',
+    './brands/spotify.png',
+    './amozesh/install.mp4',
+    './amozesh/spotify.mp4',
 ]
 
 
 
 
-const PRECACHE = 'v6';
+const PRECACHE = 'v1.2';
 const RUNTIME = 'runtime';
 
 
