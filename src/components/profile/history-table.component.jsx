a95616bc36497983f134ed3ebbc3760e42311a4b
MahdyarEn <mahdyarEn@gmail.com>
2022-06-16 09:25:10 +0430

    responsive profile

--- src/components/profile/history-table.component.jsx
+++ src/components/profile/history-table.component.jsx
@@ -1,29 +1,24 @@
-
-import { HistoryItem } from './item-history.component';
+import { HistoryItem } from "./item-history.component";
 export function HistoryTable(props) {
-    const downloads = props.downloads;
-    return (
-
-        <table className="table-auto text-gray-400 border-separate space-y-6 text-sm min-w-full">
-
-            <thead className="bg-gray-800 text-gray-500">
-                <tr>
-                    <th className="p-3">#</th>
-                    <th className="p-3 text-left">نام فایل</th>
-                    <th className="p-3">آیپی</th>
-                    <th className="p-3 text-left">پلتفرم</th>
-                    <th className="p-3 text-left">لینک دریافتی</th>
-                    <th className="p-3 text-left">موفقیت آمیز؟</th>
-                    <th className="p-3 text-left">تاریخ</th>
-                </tr>
-            </thead>
-            <tbody>
-                {downloads.map((item, index) => {
-                    return <HistoryItem key={index + 1} index={index} item={item} />
-                })}
-
-            </tbody>
-        </table>
-
-    )
+  const downloads = props.downloads;
+  return (
+    <table className="table-auto text-gray-400 border-separate space-y-6 text-sm min-w-full">
+      <thead className="bg-gray-800 text-gray-500">
+        <tr>
+          <th className="p-3">#</th>
+          <th className="p-3 text-left">نام فایل</th>
+          <th className="p-3">آیپی</th>
+          <th className="p-3 text-left">پلتفرم</th>
+          <th className="p-3 text-left">لینک دریافتی</th>
+          <th className="p-3 text-left">موفقیت آمیز؟</th>
+          <th className="p-3 text-left">تاریخ</th>
+        </tr>
+      </thead>
+      <tbody>
+        {downloads.map((item, index) => {
+          return <HistoryItem key={index + 1} index={index} item={item} />;
+        })}
+      </tbody>
+    </table>
+  );
 }
--- src/pages/profile.page.jsx
+++ src/pages/profile.page.jsx
@@ -1,128 +1,101 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpotify } from '@fortawesome/fontawesome-free-brands'
-import { useContext, useEffect, useState } from 'react';
-import { Redirect } from 'react-router-dom';
-import { LoadingContext } from 'react-router-loading';
-import AuthContext from '../contexts/auth.context';
-import { infoStore } from '../store/info.store';
-import { PageWrapper } from '../Wrappers/pages.wrapper'
-import { Table, Avatar, Badge } from 'react-daisyui'
-import moment from 'moment'
-import { userService } from '../service/index.service';
-import { HistoryTable } from '../components/profile/history-table.component';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpotify } from "@fortawesome/fontawesome-free-brands";
+import { useContext, useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import { LoadingContext } from "react-router-loading";
+import AuthContext from "../contexts/auth.context";
+import { infoStore } from "../store/info.store";
+import { PageWrapper } from "../Wrappers/pages.wrapper";
+import { Table, Avatar, Badge } from "react-daisyui";
+import moment from "moment";
+import { userService } from "../service/index.service";
+import { HistoryTable } from "../components/profile/history-table.component";
 export function ProfilePage() {
-    const loadingContext = useContext(LoadingContext);
-    const [downloads, setDownloads] = useState([]);
-    useEffect(() => {
-        document.title = `${infoStore.brandName.fa} - پروفایل`;
-        loadingContext.done();
-    }, [])
+  const loadingContext = useContext(LoadingContext);
+  const [downloads, setDownloads] = useState([]);
+  useEffect(() => {
+    document.title = `${infoStore.brandName.fa} - پروفایل`;
+    loadingContext.done();
+  }, []);
 
-    useEffect(() => {
-        async function getDownloads() {
-            const downloads = await userService.getDownloads()
-            setDownloads(downloads)
-        }
-        getDownloads()
-    }, [])
-    const authContext = useContext(AuthContext);
-    if (!authContext.isAuthenticated) {
-        return <Redirect to="/" />
+  useEffect(() => {
+    async function getDownloads() {
+      const downloads = await userService.getDownloads();
+      setDownloads(downloads);
     }
+    getDownloads();
+  }, []);
+  const authContext = useContext(AuthContext);
+  if (!authContext.isAuthenticated) {
+    return <Redirect to="/" />;
+  }
 
+  return (
+    <PageWrapper>
+      <div className=" shadow-md rounded-3xl lg:flex-row dark:bg-zinc-900/95">
+        <main className="p-6 lg:py-8 lg:px-10 rounded-3xl dark:bg-zinc-900/95 min-h-screen">
+          <div className="hero  ">
+            <div className="hero-content text-center">
+              <div className="flex items-center justify-center">
+                <div className="items-center">
+                  <div className="stats stats-vertical lg:stats-horizontal  shadow-lg">
+                    <div className="stat">
+                      <div className="stat-title">پروفایل</div>
 
-    return (
-        <PageWrapper>
-            <div className=" shadow-md rounded-3xl lg:flex-row dark:bg-zinc-900/95">
-                <main className="p-6 lg:py-8 lg:px-10 rounded-3xl dark:bg-zinc-900/95">
-                    <div className='hero min-h-screen '>
-                        <div className="hero-content text-center">
-                            <div className="flex items-center justify-center">
+                      <div className="stat-title">{authContext.user.username}</div>
+                      <div className="stat-desc ">{authContext.user.email}</div>
 
-                                <div className="items-center">
-
-
-
-                                    <div className="stats stats-vertical lg:stats-horizontal  shadow-lg">
-                                        <div className="stat">
-                                            <div className="stat-title">
-                                                پروفایل
-
-                                            </div>
-
-                                            <div className="stat-title">
-                                                {authContext.user.username}
-                                            </div>
-                                            <div className="stat-desc ">
-                                                {authContext.user.email}
-                                            </div>
-
-                                            <div className="stat-figure ">
-                                                <div className="avatar online">
-                                                    <div className="w-14 rounded-full">
-                                                        <img src={authContext.user.avatar} />
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                        </div>
-
-
-                                        <div className="stat">
-                                            <div className="stat-figure ">
-                                                <FontAwesomeIcon icon={['fas', 'star']} size={'2x'}></FontAwesomeIcon>
-                                            </div>
-                                            <div className="stat-title">وضعیت اشتراک</div>
-                                            <div className="stat-value ">ندارید</div>
-                                        </div>
-
-                                        <div className="stat">
-                                            <div className="stat-figure ">
-                                                <FontAwesomeIcon icon={['fas', 'download']} size={'2x'}></FontAwesomeIcon>
-                                            </div>
-                                            <div className="stat-title">تعداد دانلود</div>
-                                            <div className="stat-value ">{downloads.length}</div>
-                                        </div>
-                                        <div className="stat">
-                                            <div className="stat-figure ">
-                                                <FontAwesomeIcon icon={faSpotify} size={'2x'}></FontAwesomeIcon>
-                                            </div>
-                                            <div className="stat-title">وضعیت  اکانت اسپاتیفای</div>
-                                            <div className="stat-desc ">
-                                                <span className='bg-red-400 text-gray-50 rounded-md px-5 h-5'>غیرفعـال</span>
-                                            </div>
-                                        </div>
-
-
-
-                                    </div>
-
-
-
-
-                                    <div className="mt-5">
-                                        <h3 className="text-2xl font-bold mb-2 mt-2"> دانلودها</h3>
-                                        <hr className="border-gray-700" />
-                                        <div className='mt-3 w-full mb-8 overflow-hidden rounded-lg shadow-lg'>
-
-                                            <div className='flex flex-col lg:flex-row'>
-                                                <div className='flex-1'>
-                                                    <HistoryTable downloads={downloads} />
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-
-                                </div>
-
-                            </div>
+                      <div className="stat-figure ">
+                        <div className="avatar online">
+                          <div className="w-14 rounded-full">
+                            <img src={authContext.user.avatar} />
+                          </div>
                         </div>
+                      </div>
                     </div>
 
-                </main>
+                    <div className="stat">
+                      <div className="stat-figure ">
+                        <FontAwesomeIcon icon={["fas", "star"]} size={"2x"}></FontAwesomeIcon>
+                      </div>
+                      <div className="stat-title">وضعیت اشتراک</div>
+                      <div className="stat-value ">ندارید</div>
+                    </div>
+
+                    <div className="stat">
+                      <div className="stat-figure ">
+                        <FontAwesomeIcon icon={["fas", "download"]} size={"2x"}></FontAwesomeIcon>
+                      </div>
+                      <div className="stat-title">تعداد دانلود</div>
+                      <div className="stat-value ">{downloads.length}</div>
+                    </div>
+                    <div className="stat">
+                      <div className="stat-figure ">
+                        <FontAwesomeIcon icon={faSpotify} size={"2x"}></FontAwesomeIcon>
+                      </div>
+                      <div className="stat-title">وضعیت اکانت اسپاتیفای</div>
+                      <div className="stat-desc ">
+                        <span className="bg-red-400 text-gray-50 rounded-md px-5 h-5">غیرفعـال</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-        </PageWrapper>
-    )
+          </div>
+          <div className="mt-5 max-w-7xl m-auto text-center">
+            <h3 className="text-2xl font-bold mb-2 mt-2"> دانلودها</h3>
+            <hr className="border-gray-700" />
+            <div className="mt-3 w-full mb-8 overflow-x-auto rounded-lg shadow-lg">
+              <div className="flex flex-col lg:flex-row">
+                <div className="flex-1">
+                  <HistoryTable downloads={downloads} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </PageWrapper>
+  );
 }
