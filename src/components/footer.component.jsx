e85ee6a81196d01bec8cbe109b9325ade637db15
sajjadmrx <sajjadzibaee102@gmail.com>
2022-08-04 14:19:02 +0430

    change fontSize

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -5,7 +5,7 @@ import {infoStore} from "../store/info.store";
 export function FooterComponent() {
     const {isAuthenticated, setIsAuthenticated} = useContext(AuthContext);
     return (
-        <footer className="footer footer-center p-4 bg-base-300 text-base-content text-lg mt-2">
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content text-sm mt-2">
             <div>
                 <p>کليه حقوق اين سایت متعلق به ایزی دانلود می باشد و هر گونه کپی برداری از سایت غیر مجاز و بدون رضایت
                     ماست.</p>
