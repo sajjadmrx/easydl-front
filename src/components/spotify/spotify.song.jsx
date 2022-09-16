d0b55972fc866a3d49d6f657290787e9e46c6a47
sajjadmrx <sajjadzibaee102@gmail.com>
2022-09-16 21:30:09 +0430

    update download btn

--- src/components/spotify/spotify.song.jsx
+++ src/components/spotify/spotify.song.jsx
@@ -20,11 +20,10 @@ export function SpotifySongComponent(props) {
           بیشترین دانلود
         </span>
       )}
-
       <img
         className="object-cover w-32 h-32 rounded-full ring-4 mask mask-hexagon"
         src={photo}
-        alt=""
+        alt={song.artist}
       />
 
       <h1 className="mt-4 text-2xl font-semibold  capitalize dark:text-white group-hover:text-white">
@@ -45,29 +44,14 @@ export function SpotifySongComponent(props) {
               ></button>
             </div>
           ) : (
-            <a
-              href="javascript: void(0)"
+            <button
               onClick={() => downloadHandler(setValueProgress, setWiting)}
-              className="mx-2 text-gray-500 dark:text-gray-300 group-hover:text-gray-300"
+              className="btn btn-ghost mx-2 text-gray-500 dark:text-gray-300 group-hover:text-gray-300"
               style={{ display: valueProgress > 0 ? "none" : "block" }}
             >
-              <svg
-                className="icon"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                <polyline points="7 10 12 15 17 10" />
-                <line x1="12" y1="15" x2="12" y2="3" />
-              </svg>
-            </a>
+              <FontAwesomeIcon icon={["fas", "music"]} className={"mr-2"} />
+              <span>دانلود</span>
+            </button>
           )}
         </div>
         <ProgressDownload valueProgress={valueProgress} />
