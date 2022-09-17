52421d152d25fbd39821a2b025acca5d12479e18
sajjadmrx <sajjadzibaee102@gmail.com>
2022-09-17 14:38:55 +0430

    update footer

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,15 +1,29 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import AuthContext from "../contexts/auth.context";
-import {infoStore} from "../store/info.store";
+import { infoStore } from "../store/info.store";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function FooterComponent() {
-    const {isAuthenticated, setIsAuthenticated} = useContext(AuthContext);
-    return (
-        <footer className="footer footer-center p-4 bg-base-300 text-base-content text-sm mt-2">
-            <div>
-                <p>کليه حقوق اين سایت متعلق به ایزی دانلود می باشد و هر گونه کپی برداری از سایت غیر مجاز و بدون رضایت
-                    ماست.</p>
-            </div>
-        </footer>
-    )
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+  return (
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+      <div>
+        <p>شبکه های اجتماعی</p>
+        <div className="grid grid-flow-col gap-4">
+          <a href={"https://discord.gg/EXfZkRHqwq"} target={"_blank"}>
+            <FontAwesomeIcon icon={["fab", "discord"]} size={"2x"} />
+          </a>
+          <a href={"https://t.me/easydl_support"} target={"_blank"}>
+            <FontAwesomeIcon icon={["fab", "telegram"]} size={"2x"} />
+          </a>
+        </div>
+      </div>
+      <div>
+        <p>
+          کليه حقوق اين سایت متعلق به ایزی دانلود می باشد و هر گونه کپی برداری
+          از سایت غیر مجاز و بدون رضایت ماست.
+        </p>
+      </div>
+    </footer>
+  );
 }
